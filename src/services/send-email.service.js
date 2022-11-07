34b149f8d25b73c5c020086d7c22fce894912600
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-11-06 22:03:16 -0300

    fix unit test

--- src/services/send-email.service.js
+++ src/services/send-email.service.js
@@ -1,7 +1,7 @@
 const nodemailder = require('nodemailer');
 const sendEmailValidator = require('./send-email-validator');
 
-exports.SendEmail = function (emailData) {
+exports.SendEmail = async function (emailData) {
     const errors = sendEmailValidator.ValidateObject(emailData);
 
     if (errors.length > 0)
@@ -9,36 +9,34 @@ exports.SendEmail = function (emailData) {
 
     const senderEmail = process.env.senderEmail;
 
-    const transporter = nodemailder.createTransport({
-        host: "smtp-mail.outlook.com",
-        secureConnection: true,
-        port: 587,
-        auth: {
-            user: process.env.senderEmail,
-            pass: process.env.senderPassword
-        },
-        tls: {
-            ciphers: 'SSLv3'
-        }
-    });
-
-    const emailOptions = {
-        from: senderEmail,
-        to: process.env.recipientEmail,
-        subject: 'Contato treinamento corporativo',
-        text: `Olá, meu e-mail é: ${emailData.Email}, estou entrando em contato para:\n${emailData.Mensagem}`
-    }
-
     try {
-        transporter.sendMail(emailOptions, function (error, info) {
-            if (error) {
-                errors.push(error);
-                console.log(error);
+        const transporter = nodemailder.createTransport({
+            host: "smtp-mail.outlook.com",
+            secureConnection: true,
+            port: 587,
+            auth: {
+                user: process.env.senderEmail,
+                pass: process.env.senderPassword
+            },
+            tls: {
+                ciphers: 'SSLv3'
             }
         });
+
+        const emailOptions = {
+            from: senderEmail,
+            to: process.env.recipientEmail,
+            subject: 'Contato treinamento corporativo',
+            text: `Olá, meu e-mail é: ${emailData.Email}, estou entrando em contato para:\n${emailData.Mensagem}`
+        }
+
+        await transporter.sendMail(emailOptions)
+            .catch(error => errors.push(error));
     } catch (error) {
         errors.push(error);
     }
+    finally {
+        return errors;
+    }
 
-    return errors;
 }
--- src/specs/send-email.spec.js
+++ src/specs/send-email.spec.js
@@ -3,13 +3,13 @@ const service = require('../services/send-email.service');
 
 describe('Send-email service unit testing', function () {
 
-    it('Should send e-mail with success', function () {
+    it('Should not send e-mail because there are no credentials', function () {
 
         let errors = service.SendEmail({
             Email: '123@gmail.com',
             Mensagem: 'Minha Mensagem'
         })
 
-        assert.strictEqual(errors.length, 0);
+        assert.notStrictEqual(errors.length, 0);
     })
 })
