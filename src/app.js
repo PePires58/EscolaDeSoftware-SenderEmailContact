c553f2bff1ba8cef96dde20e39a20fb649da3171
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-11-07 19:40:00 -0300

    changing for promise

--- src/app.js
+++ src/app.js
@@ -4,19 +4,24 @@ exports.lambdaHandler = async (event, context) => {
 
     try {
         const body = JSON.parse(event.body);
+        console.log('body' + body);
 
         const errors = [];
 
+        console.log('sending e-mail');
         sendEmailService.SendEmail({
             Email: body.Email,
             Mensagem: body.Mensagem
         }).then((result) => {
+            console.log('e-mail sended');
             errors.push(result);
         })
             .catch((error) => {
+                console.log('error on send e-mail');
                 errors.push(error);
             });
 
+        console.log('returning result');
         return errors.length > 0 ? defaultResult(400, 'Erro ao enviar o e-mail') :
             defaultResult(200, 'E-mail enviado com sucesso');
     } catch (error) {
