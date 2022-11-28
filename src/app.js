b48d7fc381ccaef9fcbf7768b6095a45b0716f24
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-11-28 08:11:53 -0300

    initial version

--- src/app.js
+++ src/app.js
@@ -3,44 +3,48 @@ const sendEmailService = require('./services/send-email.service');
 exports.lambdaHandler = async (event, context) => {
 
     try {
+        if (event.headers['access-control-allow-origin'] === 'https://dak1pni58hzx7.cloudfront.net') {
+            const body = JSON.parse(event.body);
+            const errors = [];
 
-        console.log(event);
+            await sendEmailService.SendEmail({
+                Email: body.Email,
+                Mensagem: body.Mensagem
+            }).then(() => {
+                console.log('e-mail sended');
+            })
+                .catch((error) => {
+                    console.log('error on send e-mail');
+                    console.log(error);
+                    errors.push(error);
+                });
 
-        const body = JSON.parse(event.body);
-        const errors = [];
-
-        await sendEmailService.SendEmail({
-            Email: body.Email,
-            Mensagem: body.Mensagem
-        }).then(() => {
-            console.log('e-mail sended');
-        })
-            .catch((error) => {
-                console.log('error on send e-mail');
-                console.log(error);
-                errors.push(error);
-            });
-
-        return errors.length > 0 ? defaultResult(400, 'Erro ao enviar o e-mail') :
-            defaultResult(200, 'E-mail enviado com sucesso')
-
+            return errors.length > 0 ? defaultResult(400, 'Erro ao enviar o e-mail') :
+                defaultResult(200, 'E-mail enviado com sucesso')
+        }
+        return errorResult(403, 'Erro de CORS');
     } catch (error) {
         return errorResult(error);
     }
 }
 
-function errorResult(errors) {
+function errorResult(statusCode, errors) {
     return {
-        'statusCode': 500,
+        'statusCode': statusCode,
         'body': JSON.stringify({
             errors: errors
         }),
         'isBase64Encoded': false,
         'headers': {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "https://dak1pni58hzx7.cloudfront.net",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
         }
     };
 }
+
+function errorResult(errors) { return errorResult(500, errors); }
 
 function defaultResult(statusCode, message) {
     return {
@@ -50,7 +54,10 @@ function defaultResult(statusCode, message) {
         }),
         'isBase64Encoded': false,
         'headers': {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "https://dak1pni58hzx7.cloudfront.net",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
         }
     }
 }
