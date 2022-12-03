abbcff7c8c99e95f0cf6bda01b61e03e4ac5b6ba
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-12-02 21:05:03 -0300

    removing base64

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ const defaultCors = cors({
 
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(defaultCors);
 
-app.options("/email", defaultCors);
 
 app.use("/email", emailRouter);
 
